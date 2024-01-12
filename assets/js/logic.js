3661a706fb8908a3b096a707be9679d7b200f178
Steve Hannay <hannay.steve@googlemail.com>
2024-01-12 20:08:54 +0000

    pseudocode added to js logic file

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,67 @@
+/* 
+    Javascript for Challenge 06 
+    Developer - Steve Hannay 
+    First Created - 12th January 2024
+
+    This code handles logic for the quiz
+    Questions and Answers for the quiz are located in the javascript file questions.js
+
+    
+
+*/
+
+alert(javacriptQuestions[0].question)
 
 
-alert("TEST")
+// Define Variables
+
+    // create references to html elements
+
+
+// Add Event Listeners for Buttons and List
+
+
+
+
+// Start Quiz (triggered by "Start" button)
+    // reset variables and screen
+    // Start Timer
+    // Display Question
+
+
+// Check User Answer (triggered by List event - via button click)
+    // get user answer from button that was clicked
+    // compare user answer with correct answer
+    // update scores
+    
+    // if user has answered all questions then
+        // End Quiz
+    
+    
+    // if timer still running then 
+        // display next question
+    // else 
+        //  End Quiz
+
+
+
+// End Quiz
+    // Clear Question Buttons
+    // display score and give the user the ability to save their initials and their score
+
+
+    
+// Utility functins :
+
+// Display Question 
+    // Clear Question Buttons
+    // Get Next Questions ----> this can later be randomised
+
+// Clear Question
+
+
+// Game Timer     
+    // Countdown to zero from 
+    // End Quiz is timer reaches zero
+
+
---
+++ assets/js/questions.js
@@ -0,0 +1,34 @@
+/* 
+    Javascript for Challenge 06 
+    Developer - Steve Hannay 
+    First Created - 12th January 2024
+
+    This code handles the storage of Questions and Answers to be used by the quiz
+    Logic for the quiz is located in the javascript file logic.js
+
+*/
+
+
+var javacriptQuestions = [
+	{
+		question: "QUESTION 1?",
+		selectableAnswers: {
+			a: '3',
+			b: '5',
+			c: '115'
+		},
+		correctAnswer: 'b'
+	},
+	{
+		question: "QUESTION 2?",
+		selectableAnswers: {
+			a: '3',
+			b: '5',
+			c: '10',
+            d: ''
+		},
+		correctAnswer: 'c'
+	}
+];
+
+
